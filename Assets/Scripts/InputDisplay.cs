15cc304182caad09d3cb8ccc4b64e321d6b702e2
L4mbads <rizacal.mamen@gmail.com>
2024-01-30 14:48:12 +0700

    arrow sprites

--- Assets/Scripts/InputDisplay.cs
+++ Assets/Scripts/InputDisplay.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class InputDisplay : MonoBehaviour
 {
-    [SerializeField] GameObject A;
-    [SerializeField] GameObject B;
-    [SerializeField] GameObject C;
-    [SerializeField] GameObject D;
+    [SerializeField] SpriteRenderer A;
+    [SerializeField] SpriteRenderer B;
+    [SerializeField] SpriteRenderer C;
+    [SerializeField] SpriteRenderer D;
+    [SerializeField] Sprite Up;
+    [SerializeField] Sprite Down;
+    [SerializeField] Sprite Left;
+    [SerializeField] Sprite Right;
     // Start is called before the first frame update
     private static float[] rot = new float[4]{0.0f, 90.0f, 180.0f, 270.0f};
-    private GameObject[] display = new GameObject[4];
+
+    private static Sprite[] sprites = new Sprite[4];
+    private SpriteRenderer[] display = new SpriteRenderer[4];
 
     void Awake()
     {
@@ -18,6 +24,12 @@ public class InputDisplay : MonoBehaviour
         display[1] = B;
         display[2] = C;
         display[3] = D;
+
+        sprites[0] = Up;
+        sprites[1] = Left;
+        sprites[2] = Down;
+        sprites[3] = Right;
+
     }
 
     public void SetUp()
@@ -25,9 +37,10 @@ public class InputDisplay : MonoBehaviour
 
         for(int i = 0; i < 4; ++i)
         {
-            display[i].SetActive(true);
+            display[i].gameObject.SetActive(true);
+            display[i].sprite = sprites[GameLogic.inputs[i]-1];
             //display[i].transform.Rotate(0.0f, 0.0f, rot[GameLogic.inputs[i]-1], Space.World);
-            display[i].transform.localRotation = Quaternion.Euler(0, 0, rot[GameLogic.inputs[i]-1]);
+            //display[i].transform.localRotation = Quaternion.Euler(0, 0, rot[GameLogic.inputs[i]-1]);
         }
 
     }
@@ -40,7 +53,7 @@ public class InputDisplay : MonoBehaviour
         {
             if(GameLogic.inputs[i] == 0)
             {
-                display[i].SetActive(false);
+                display[i].gameObject.SetActive(false);
             }
         }
 
