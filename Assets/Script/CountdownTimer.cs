c86e36d31ea8188ac3a121004150f52b9867d59c
Rafizan <rafi.syawalazmi@gmail.com>
2024-02-01 08:56:29 +0700

    Tap Phase Changes
    Huge changes I forgot what it were.

--- Assets/Script/CountdownTimer.cs
+++ Assets/Script/CountdownTimer.cs
@@ -8,16 +8,25 @@ public class CountdownTimer : MonoBehaviour
     public int time {get; private set;}
     [SerializeField] TextMeshProUGUI timeText;
     [SerializeField] int startingTime;
+    private int cardTime1;
+    private int cardTime2;
 
     void Start()
     {
         time = startingTime;
+
+        cardTime1 = Random.Range(45, 50);
+        cardTime2 = Random.Range(15, 20);
         InvokeRepeating("TimerDecrease", 1f, 1f);
     }
 
     void TimerDecrease()
     {
         time--;
+        if (time == cardTime1 | time == cardTime2)
+        {
+            GetComponent<CardEvent>().TriggerCardEvent();
+        }
         UpdateUI();
     }
 
